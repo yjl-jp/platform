5535744a7661021df18c3e1ba3e1a92778383aa9
yjl-jp <yingjingliang24@gmail.com>
2025-08-02 22:07:50 +0800

    idle and animations

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,19 +3,30 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
     [SerializeField] private float moveSpeed;
 
     private float xInput;
+   
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
     private void Update()
     {
         xInput = Input.GetAxis("Horizontal");
+        HandleAnimations();
         HandleMovement();
-        
+
     }
+
+    private void HandleAnimations()
+    {
+        
+        anim.SetBool("isRunning", rb.linearVelocityX != 0);
+    }
+
     private void HandleMovement()
     {
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
